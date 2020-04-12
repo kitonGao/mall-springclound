64e326424e5cc4b4fa18337912db570d32b2ff15
schindler <15822985265@163.com>
2020-04-12 23:13:44 +0800

    数据库的实体类和mapper文件

---
+++ mall-mbg/src/main/java/com/mall/mbg/Generator.java
@@ -0,0 +1,50 @@
+package com.mall.mbg;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈用于生产MBG代码〉
+ *
+ * @author schindler
+ * @create 2020/4/12 23:04
+ * @since 1.0.0
+ * 备注:写这段代码的时候，只有上帝和我知道它是干嘛的。现在，只有上帝知道。
+ */
+public class Generator {
+    public static void main(String[] args) throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
+        //MBG 执行过程中的警告信息
+        List<String> warnings = new ArrayList<String>();
+        //当生成的代码重复时，覆盖原代码
+        boolean overwrite = true;
+        //读取我们的 MBG 配置文件
+        InputStream is = Generator.class.getResourceAsStream("/generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(is);
+        is.close();
+
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        //创建 MBG
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        //执行生成代码
+        myBatisGenerator.generate(null);
+        //输出警告信息
+        for (String warning : warnings) {
+            System.out.println(warning);
+        }
+    }
+
+
+}
+
---
+++ mall-mbg/src/main/java/com/mall/mbg/mapper/OmsOrderSettingMapper.java
@@ -0,0 +1,30 @@
+package com.mall.mbg.mapper;
+
+import com.mall.mbg.entity.OmsOrderSetting;
+import com.mall.mbg.entity.OmsOrderSettingExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface OmsOrderSettingMapper {
+    long countByExample(OmsOrderSettingExample example);
+
+    int deleteByExample(OmsOrderSettingExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(OmsOrderSetting record);
+
+    int insertSelective(OmsOrderSetting record);
+
+    List<OmsOrderSetting> selectByExample(OmsOrderSettingExample example);
+
+    OmsOrderSetting selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") OmsOrderSetting record, @Param("example") OmsOrderSettingExample example);
+
+    int updateByExample(@Param("record") OmsOrderSetting record, @Param("example") OmsOrderSettingExample example);
+
+    int updateByPrimaryKeySelective(OmsOrderSetting record);
+
+    int updateByPrimaryKey(OmsOrderSetting record);
+}
